a6d4c683ab4370fbf00cadc7add17ec332f3cc33
shahin <shahin.parveen@ejamerica.com>
2024-06-24 13:56:39 +0530

    added cal c

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
+import { useState } from 'react';
 import './App.css';
-import Tooltip from './Tooltip';
-
 function App() {
+
+  const arr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+    '+', '-', '/', '*', '=', 'C', '.'
+  ]
+  const [value, setValue] = useState('');
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setValue(e.target.value);
+  }
+  const handleClick = (e) => {
+    const id = e.target.id;
+    if (id === 'C') {
+      setValue('');
+    } else if (id === '=') {
+      //produce a result
+      handleSubmit();
+    } else {
+      setValue((val) => val + id);
+    }
+    console.log();
+  }
+
+  const handleSubmit = (e) => {
+    e?.preventDefault();
+    try {
+      const ans = eval(value);
+      setValue(ans);
+    } catch (err) {
+      alert("Invalid Inputs")
+    }
+  }
+  console.log(value);
   return (
     <div className="App">
-      <h1>Tooltip Demo</h1>
-      <Tooltip position="top" text="Tooltip on top">
-        <button>Hover me (top)</button>
-      </Tooltip>
-      <Tooltip position="bottom" text="Tooltip on bottom">
-        <button>Hover me (bottom)</button>
-      </Tooltip>
-      <Tooltip position="left" text="Tooltip on left">
-        <button>Hover me (left)</button>
-      </Tooltip>
-      <Tooltip position="right" text="Tooltip on right">
-        <button>Hover me (right)</button>
-      </Tooltip>
+      <h1>Calculator</h1>
+      <form
+        onSubmit={handleSubmit}
+      >
+        <input
+          value={value}
+          onChange={handleChange}
+          type='text'
+        />
+      </form>
+      <div className='container'
+        onClick={handleClick}
+      >
+        {
+          arr.map((item, idx) => (
+            <button
+              id={item}
+              key={idx}
+              className='cell'
+            >{item}</button>
+          ))
+        }
+      </div>
     </div>
   );
 }
